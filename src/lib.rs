8ca7ed8e4f38d4a3b4b8ae89c7fbe4e36af39e54
matthew-matvei <matthew.d.james87@gmail.com>
2026-01-23 17:30:15 +1100

    Use insta for snapshot testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,46 @@
-pub struct GameBoard {}
+pub struct GameBoard<'a, T: RendersGameBoard> {
+    width: u8,
+    height: u8,
+    renderer: &'a T,
+}
 
-impl GameBoard {
-    pub fn render(&self) -> Vec<Vec<String>> {
-        vec![vec!["".to_string()]]
+impl<'a, T: RendersGameBoard> GameBoard<'a, T> {
+    pub fn render(&self) -> () {
+        let mut instructions = Vec::new();
+        for _row in 0..self.height {
+            instructions.push(RenderInstruction::Character('|'));
+            for _col in 0..self.width {
+                instructions.push(RenderInstruction::Character(' '));
+            }
+            instructions.push(RenderInstruction::Character('|'));
+            instructions.push(RenderInstruction::NextLine);
+        }
+
+        instructions.push(RenderInstruction::Character('|'));
+
+        for _col in 0..self.width {
+            instructions.push(RenderInstruction::Character('_'));
+        }
+
+        instructions.push(RenderInstruction::Character('|'));
+
+        self.renderer.render(instructions);
     }
+
+    pub fn new(width: u8, height: u8, renderer: &'a T) -> GameBoard<'a, T> {
+        GameBoard {
+            width,
+            height,
+            renderer,
+        }
+    }
+}
+
+pub enum RenderInstruction {
+    Character(char),
+    NextLine,
+}
+
+pub trait RendersGameBoard {
+    fn render(&self, instructions: Vec<RenderInstruction>);
 }
--- tests/game_board_tests.rs
+++ tests/game_board_tests.rs
@@ -1,12 +1,41 @@
+use std::cell::RefCell;
+
 use rusty_blocks::*;
 
 #[test]
 fn it_renders_an_empty_board_after_initialising() {
-    let rendered_board = GameBoard {}.render();
+    let renderer = TestRenderer::new();
+    GameBoard::new(10, 20, &renderer).render();
 
-    for row in &rendered_board {
-        for element in row {
-            assert_eq!(element, "")
+    insta::assert_snapshot!(renderer.get_snapshot());
+}
+
+#[derive(Clone)]
+struct TestRenderer {
+    snapshot: RefCell<String>,
+}
+
+impl TestRenderer {
+    fn get_snapshot(&self) -> String {
+        self.snapshot.borrow().to_string()
+    }
+
+    fn new() -> TestRenderer {
+        TestRenderer {
+            snapshot: RefCell::new(String::new()),
+        }
+    }
+}
+
+impl RendersGameBoard for TestRenderer {
+    fn render(&self, instructions: Vec<rusty_blocks::RenderInstruction>) {
+        for instruction in &instructions {
+            match instruction {
+                RenderInstruction::Character(character) => {
+                    self.snapshot.borrow_mut().push(*character)
+                }
+                RenderInstruction::NextLine => self.snapshot.borrow_mut().push('\n'),
+            }
         }
     }
 }
